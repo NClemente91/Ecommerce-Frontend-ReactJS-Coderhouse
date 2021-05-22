2c058aeaad1fd64e73a13fa3087b4dcd9353ac0c
Nicolás Clemente <nicoclemente91@gmail.com>
2021-05-22 20:56:08 -0300

    Primer Commit - Desafio 1

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import './styles/App.css';
+import logoSG from '../images/LogoPpal.svg'
+
+function App() {
+  return (
+    <div className="App">
+      <img src={logoSG} alt="Logo SG Congelados"/>
+      <h1>¡Hacer platos ricos, sanos y y en poco tiempo es posible. Animate a comprobarlo!</h1>
+    </div>
+  );
+}
+
+export default App;
